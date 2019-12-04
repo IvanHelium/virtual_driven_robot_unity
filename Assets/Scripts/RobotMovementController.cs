a74c01c73bc3ba6947edf4ae4ae02562c26e32f7
Ivan <vangan9999@gmail.com>
2019-12-04 22:48:34 +0300

    fixed moving and rotating

--- Assets/Scripts/RobotMovementController.cs
+++ Assets/Scripts/RobotMovementController.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class RobotMovementController : MonoBehaviour
 {
     private Vector2 _vector2_curr_dir;
+    private Vector2 _vector2_dir_north = new Vector2(0.0f, 1.0f); //but it have to be const
     public float _speed_scale = 0.01f;
     private Rigidbody2D _rigidbody2D;
 
@@ -16,6 +17,7 @@ public class RobotMovementController : MonoBehaviour
     private float remain_movement_distance = 0.0f;
     private float movement_distance = 4.0f;
 
+    private Vector2 getVector; //vector between our movement direction (_vector2_curr_dir) and vector of robot rigidbody (it is not same)
 
 
     // Start is called before the first frame update
@@ -23,11 +25,10 @@ public class RobotMovementController : MonoBehaviour
     {
         _rigidbody2D = this.GetComponent<Rigidbody2D>();
 
-        _vector2_curr_dir = new Vector2(0.0f, 1.0f); // our robot forward vector is north (up)
+        _vector2_curr_dir = _vector2_dir_north; // our robot forward vector is north (up)
 
 
-        action_rotate_left_45_and_move_forward();
-
+        
     }
 
     // Update is called once per frame
@@ -40,13 +41,21 @@ public class RobotMovementController : MonoBehaviour
     {
         rotate_and_move_tick();
 
-        action_rotate_left_45_and_move_forward();
+        //action_move_forward();
+        action_rotate_left_90_and_move_forward();
+
+
     }
 
-
+    //----------------------------------------------------------------------
+    //----------------------------------------------------------------------
+    //
+    // different actions
+    //
+    //----------------------------------------------------------------------
+    //----------------------------------------------------------------------
 
     void action_rotate_left_45_and_move_forward(){
-
         if(isMovingStarted || isRotationStarted) {
             return;
         }
@@ -54,7 +63,65 @@ public class RobotMovementController : MonoBehaviour
         move_forward_start(movement_distance, _speed_scale);
     }
 
+    void action_rotate_left_90_and_move_forward()
+    {
+        if (isMovingStarted || isRotationStarted)
+        {
+            return;
+        }
+        rotate_robot_start(_vector2_curr_dir, 90.0f, _speed_scale);
+        move_forward_start(movement_distance, _speed_scale);
+    }
+    void action_rotate_left_135_and_move_forward()
+    {
+        if (isMovingStarted || isRotationStarted)
+        {
+            return;
+        }
+        rotate_robot_start(_vector2_curr_dir, 135.0f, _speed_scale);
+        move_forward_start(movement_distance, _speed_scale);
+    }
+    //----------------------------------------------------------------------
+    void action_move_forward()
+    {
+        if (isMovingStarted || isRotationStarted)
+        {
+            return;
+        }
+        rotate_robot_start(_vector2_curr_dir, 0.0f, _speed_scale);
+        move_forward_start(movement_distance, _speed_scale);
+    }
 
+    //----------------------------------------------------------------------
+    void action_rotate_right_45_and_move_forward() {
+        if (isMovingStarted || isRotationStarted) {
+            return;
+        }
+        rotate_robot_start(_vector2_curr_dir, -45.0f, _speed_scale);
+        move_forward_start(movement_distance, _speed_scale);
+    }
+
+    void action_rotate_right_90_and_move_forward()
+    {
+        if (isMovingStarted || isRotationStarted)
+        {
+            return;
+        }
+        rotate_robot_start(_vector2_curr_dir, -90.0f, _speed_scale);
+        move_forward_start(movement_distance, _speed_scale);
+    }
+
+    void action_rotate_right_135_and_move_forward()
+    {
+        if (isMovingStarted || isRotationStarted)
+        {
+            return;
+        }
+        rotate_robot_start(_vector2_curr_dir, -135.0f, _speed_scale);
+        move_forward_start(movement_distance, _speed_scale);
+    }
+
+    //----------------------------------------------------------------------
 
 
 
@@ -71,6 +138,39 @@ public class RobotMovementController : MonoBehaviour
 
 
 
+
+
+
+    void correct_rigidbody_rotation()
+    {
+        float angle_correction = 0.0f;
+        float scalar_product = 0.0f;
+        float module_a = 0.0f;
+        float module_b = 0.0f;
+        float cos_angle = 0.0f;
+
+        getVector = _rigidbody2D.GetVector(_vector2_curr_dir); 
+        
+        scalar_product = getVector.x * _vector2_dir_north.x + getVector.y * _vector2_dir_north.y;
+        module_a = Mathf.Sqrt(Mathf.Pow(getVector.x, 2.0f) + Mathf.Pow(getVector.y, 2.0f));
+        module_b = Mathf.Sqrt(Mathf.Pow(_vector2_dir_north.x, 2.0f) + Mathf.Pow(_vector2_dir_north.y, 2.0f));
+        cos_angle = scalar_product / (module_a * module_b);
+
+        angle_correction = Mathf.Acos(cos_angle);
+        angle_correction = angle_correction * Mathf.Rad2Deg;
+
+
+        if ((_vector2_dir_north.x * getVector.y - _vector2_dir_north.y * getVector.x) >= 0.0f) { //check sign of angle (clockwize = -1,  counterwize = 1)
+            _rigidbody2D.MoveRotation(_rigidbody2D.rotation + angle_correction);
+        } else {
+            _rigidbody2D.MoveRotation(_rigidbody2D.rotation - angle_correction);
+        }
+
+        
+
+
+
+    }
 
 
 
@@ -125,7 +225,10 @@ public class RobotMovementController : MonoBehaviour
             _rigidbody2D.MovePosition(_rigidbody2D.position + current_direction * movement_tick);
             remain_movement_distance = remain_movement_distance - movement_tick;
         } else {
+            //in last step our action we correct mismatch
             isMovingStarted = false;
+            
+            
         }
         
     }
@@ -142,6 +245,10 @@ public class RobotMovementController : MonoBehaviour
         float rotation_tick = 0.0f;
         Vector2 cur_dir_result;
 
+        if(angle_deg == 0.0f) {
+            return curr_dir;
+        }
+
         if (rotation_speed < 1.0f) {
             rotation_speed_cur = rotation_speed;
         }
@@ -154,8 +261,11 @@ public class RobotMovementController : MonoBehaviour
         remain_angle_deg = angle_deg;
         start_angle_deg = angle_deg;
 
-        cur_dir_result = rotate__transform_robot(curr_dir, rotation_tick);
+        cur_dir_result = rotate_transform_robot(curr_dir, rotation_tick);
+
         remain_angle_deg = angle_deg - rotation_tick;
+        
+        
 
         return cur_dir_result;
 
@@ -180,10 +290,11 @@ public class RobotMovementController : MonoBehaviour
         rotation_tick = start_angle_deg * rotation_speed;
         
 
-        if(remain_angle_deg > 0.0f){
-            cur_dir_result = rotate__transform_robot(curr_dir, rotation_tick);
+        if(remain_angle_deg > 1.0f || remain_angle_deg < -1.0f) { //epsilon angle to stop (2 degree)
+            cur_dir_result = rotate_transform_robot(curr_dir, rotation_tick);
             remain_angle_deg = remain_angle_deg - rotation_tick;
         } else {
+            correct_rigidbody_rotation();
             isRotationStarted = false;
         }
         return cur_dir_result;
@@ -191,7 +302,7 @@ public class RobotMovementController : MonoBehaviour
     }
 
 
-    Vector2 rotate__transform_robot(Vector2 curr_dir, float angle_deg)
+    Vector2 rotate_transform_robot(Vector2 curr_dir, float angle_deg)
     {
         Vector2 cur_dir_result;
         _rigidbody2D.MoveRotation( _rigidbody2D.rotation + angle_deg);
